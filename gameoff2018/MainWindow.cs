6af9660bd421c1fbe260fcd729a4a0c27833a89a
Matt <matt.ryderrule@gmail.com>
2018-11-12 18:23:02 +0000

    OpenGL context

--- gameoff2018/MainWindow.cs
+++ gameoff2018/MainWindow.cs
@@ -1,10 +1,67 @@
 ﻿using OpenTK;
+using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using System;
 
 namespace gameoff2018
 {
     public sealed class MainWindow : GameWindow
     {
+        public MainWindow(): base
+            (
+                1280, // initial width
+                720, // initial height
+                GraphicsMode.Default,
+                "Game Off 2018",  // initial title
+                GameWindowFlags.Default,
+                DisplayDevice.Default,
+                2, // OpenGL major version
+                1, // OpenGL minor version
+                GraphicsContextFlags.ForwardCompatible
+            )
+        {
+            Title += ": OpenGL Version: " + GL.GetString(StringName.Version);
+        }
 
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            CursorVisible = true;
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            HandleKeyboard();
+        }
+
+        private void HandleKeyboard()
+        {
+            var keyState = Keyboard.GetState();
+
+            if (keyState.IsKeyDown(Key.Escape))
+            {
+                Exit();
+            }
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            Title = $"(Vsync: {VSync}) FPS: {1f / e.Time:0}";
+
+            Color4 backColor;
+            backColor.A = 1.0f;
+            backColor.R = 0.1f;
+            backColor.G = 0.1f;
+            backColor.B = 0.3f;
+            GL.ClearColor(backColor);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            SwapBuffers();
+        }
     }
 }
